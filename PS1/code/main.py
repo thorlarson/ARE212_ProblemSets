3dc039b9a60bdb6c5432bdf0960f160404b8ec3a
Scott Kjorlien <skjorlien@gmail.com>
2023-03-28 16:31:42 -0700

    code pt 5

--- PS1/code/main.py
+++ PS1/code/main.py
@@ -1,33 +1,51 @@
 import numpy as np
 from scipy.stats import multivariate_normal
+from scipy.linalg import inv, sqrtm
 
+def generate_covariance_matrix(n):
+    ## creates a random covariance matrix of dimension n
+    A = np.random.random(size=(n, n))  
+    return A @ A.T
+
+def pt_5(N, k, m):
+    np.random.seed(1234)
+    '''
+        N: number of samples 
+        k: number of dependent variables (Y)
+        m: number of independent variables (X)
+    '''
+
+    ## Generate an Nxk matrix of errors 
+    mu = [0]*k
+    Sigma = generate_covariance_matrix(k)
+    u = multivariate_normal(mu, Sigma)
+
+    ## Generate an Nxm matrix for T
+    mu = [0]*m
+    Sigma = generate_covariance_matrix(m)
+    T = multivariate_normal(mu,Sigma)
+
+    ## Define the mxk beta matrix (lets just make it increasing integers starting at 1)
+    beta = np.arange(1, (m*k)+1).reshape(m, k)
+
+    ## Get the sample 
+    T = T.rvs(N)
+    u = u.rvs(N) 
+
+    # generate D and X
+    D = np.random.random(size=(m, m)) 
+    X = (T**3)@D 
+
+    print(f"True beta: {beta} \n\n")
+
+    y = X@beta + u 
+
+    b = np.linalg.lstsq(T.T@X,T.T@y, rcond=None)[0]
+    print(f"Estimated b: {b}")
 
 
 if __name__ == "__main__":
-    ## Just copied and pasted from Ethan's
-    N = 1000
-    k = 3 # Columns of Y
-   
+    pt_5(1000, 3, 2)
 
-    mu = [0]*k
-    Sigma=[[1,0.5,0],
-        [0.5,2,0],
-        [0,0,3]]
-
-    T = multivariate_normal(mu,Sigma)
-
-    u = multivariate_normal(cov=0.2)
-    beta = [1/2,1]
-
-    D = np.random.random(size=(3,2)) # Generate random 3x2 matrix
-    N=1000 # Sample size
-    # Now: Transform rvs into a sample
-    T = T.rvs(N)
-    u = u.rvs(N) # Replace u with a sample
-
-    X = (T**3)@D  # Note use of ** operator for exponentiation
-
-    y = X@beta + u # Note use of @ operator for matrix multiplication
-    print(y.shape)
     
     
