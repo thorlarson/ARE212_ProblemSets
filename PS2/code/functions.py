86c949fdf1fcfb80c646d74bf17f3ca25640d136
Scott Kjorlien <skjorlien@gmail.com>
2023-04-14 12:14:03 -0700

    update q5

--- PS2/code/functions.py
+++ PS2/code/functions.py
@@ -9,33 +9,53 @@ def dgp(n, β, π):
     Z = iid.norm(0, 1)
     u = u.rvs(n).reshape(-1, 1)
     v = v.rvs(n).reshape(-1,1)
-    Z = Z.rvs(n).reshape(-1,1)
 
-    x = π*Z + v
+    ## the number of instruments to generate = number of elements in pi
+    if type(π) == np.ndarray:
+        if π.ndim == 1: 
+            π = π.reshape(-1, 1)
+    else: 
+        π = np.array([π]).reshape(-1,1)
+    d = π.size
+    Z = Z.rvs(n*d).reshape(n, d)
+    x = Z@π + v
     y = β*x + u 
     return (y, x, Z) 
 
 
 def two_sls(y, x, Z): 
-    π = np.linalg.solve(Z.T@Z, Z.T@x) 
+    π = np.linalg.solve(Z.T@Z, Z.T@x)
     xhat = Z@π
     β = np.linalg.solve(xhat.T@xhat, xhat.T@y)
-    n = Z.shape[0]
-
     # calculate variance
+    df = Z.shape[0] - Z.shape[1]
     e = y - x@β
-    s2 = e.T@e / n 
-    V = s2 * np.linalg.inv(xhat.T@xhat)
+    V = compute_variance(e, xhat, df)
     return β, V
 
 
+def ttest(est, se, val = 0):
+    t = (est - val)/se
+    return t
 
-n = 10000
-β = 1 
-π = 1
-x, y, Z = dgp(n, β, π)
 
-b, V = two_sls(x, y, Z)
+def hansen(x, y, Z, b0):
+    ybar = y - b0*x
+    gammahat = np.linalg.solve(Z.T@Z, Z.T@ybar)
+    e = ybar - Z@gammahat
+    df = Z.shape[0] - Z.shape[1]
+    V = compute_variance(e, Z, df)
+    # define f statistic 
+    df1 = gammahat.size ## number of restrictions (setting all gamma = 0)
+    df2 = df ## N - number of parameters 
+    Q = Z@(V/Z.shape[0])@Z.T
+    fstat = (Z@gammahat).T@np.linalg.pinv(Q)@(Z@gammahat)
+    p = 2*(1 - iid.f.cdf(fstat, df1, df2))
+    return p
 
-print(b)
-print(V)
+def compute_variance(e, X, df):
+    s2 = e.T@e / df
+    V = s2 * np.linalg.inv(X.T@X)
+    return V
+
+
